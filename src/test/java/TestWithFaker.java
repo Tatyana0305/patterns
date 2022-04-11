5460b61d4c4b3eec39d67ce6d02e1495ee8ebc5d
Tatyana Bondarenko <tanya_kursavka@mail.ru>
2022-04-11 18:06:38 +0300

    optimize code

--- src/test/java/TestWithFaker.java
+++ src/test/java/TestWithFaker.java
@@ -1,39 +1,20 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.github.javafaker.Faker;
-import entitles.RegistrationInfo;
-import org.apache.commons.lang3.StringUtils;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import utils.DataGenerator;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$;
 
 public class TestWithFaker {
-    private static Faker faker;
-    RegistrationInfo info = DataGenerator.Registration
-            .generateInfo("ru");
-
-    LocalDate localDate = LocalDate.now().plusDays(3);
-    DateTimeFormatter data = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    String strData = localDate.format(data);
-
-
-    @BeforeAll
-    static void setUpAll() {
-        faker = new Faker(new Locale("ru"));
-    }
 
     @BeforeEach
-    void openBrowser(){
+    void openBrowser() {
         open("http://localhost:9999");
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1908х900";
@@ -41,92 +22,91 @@ public class TestWithFaker {
     }
 
     @Test
-    void shouldGenerateTestDataUsingUtils() {
-        RegistrationInfo info = DataGenerator.Registration
-                .generateInfo("ru");
-        printTestData(info);
-
-    }
-
-    private void printTestData(RegistrationInfo info) {
-        System.out.println(info);
-        System.out.println(StringUtils.repeat("=", 30));
-
-    }
-
-
-    @Test
     public void shouldOrderCardBySpecificDay() {
 
-        $("[data-test-id=city] input"). setValue(faker.address().city());
+        $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").val(strData);
-        $("[data-test-id=name] input").setValue(faker.name().fullName());
-        $("[data-test-id=phone] input").setValue(faker.phoneNumber().phoneNumber());
+        $("[data-test-id='date'] input").val(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(DataGenerator.generateValidName());
+        $("[data-test-id=phone] input").setValue(DataGenerator.generatePhone());
         $("[data-test-id=agreement]").click();
 
         $x("//*[text()=\"Запланировать\"]").click();
-        $("[data-test-id=success-notification]").should(Condition.visible);
-        Duration.ofSeconds(15);                      //Загрузка не более 15 секунд
+        $("[data-test-id=success-notification]").should(visible);
+
         LocalDate localDate2 = LocalDate.now().plusDays(5);
         DateTimeFormatter data2 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         String strData2 = localDate2.format(data2);
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").val(strData2);
         $("div .button").click();
-        $x("//*[@id=\"root\"]/div/div[2]/div[3]").should(Condition.visible);
-        Duration.ofSeconds(15);                      //Загрузка не более 15 секунд
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldBe(visible).shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
 
+        $(".notification_status_error .button").click();
 
-        $("[data-test-id='replan-notification'] .button").click();
-        $("[data-test-id=success-notification]").should(Condition.visible);
-        Duration.ofSeconds(15);                      //Загрузка не более 15 секунд
+        $(withText("Успешно!")).shouldBe(visible);
+        $("[data-test-id='success-notification'] .notification__content").shouldBe(visible).shouldHave
+                (exactText("Встреча успешно запланирована на " + strData2));
 
     }
 
     @Test
     public void shouldOrderCardForNotAvailableAddress() {
 
-        $("[data-test-id=city] input").setValue("Магнитогорск");
+        $("[data-test-id=city] input").setValue(DataGenerator.generateCity2());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").val(strData);
-        $("[data-test-id=name] input").setValue(faker.name().fullName());
-        $("[data-test-id=phone] input").setValue(faker.phoneNumber().phoneNumber());
+        $("[data-test-id='date'] input").val(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(DataGenerator.generateValidName());
+        $("[data-test-id=phone] input").setValue(DataGenerator.generatePhone());
         $("[data-test-id=agreement]").click();
 
         $x("//*[text()=\"Запланировать\"]").click();
-        $("[data-test-id=success-notification]").should(Condition.visible);
-        Duration.ofSeconds(15);                      //Загрузка не более 15 секунд
+        $("[data-test-id=success-notification]").should(visible);
+
+        LocalDate localDate2 = LocalDate.now().plusDays(5);
+        DateTimeFormatter data2 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String strData2 = localDate2.format(data2);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val(strData2);
+        $("div .button").click();
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldBe(visible).shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+
+        $(".notification_status_error .button").click();
+
+        $(withText("Успешно!")).shouldBe(visible);
+        $("[data-test-id='success-notification'] .notification__content").shouldBe(visible).shouldHave
+                (exactText("Встреча успешно запланирована на " + strData2));
 
     }
-    @Test
-    public void shouldOrderCardForNotAvailableAddress2() {
 
-        $("[data-test-id=city] input").setValue("Новокузнецк");
+    @Test
+    public void shouldOrderCardIfNameWidthInvalidCharacter() {
+
+        $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").val(strData);
-        $("[data-test-id=name] input").setValue(faker.name().fullName());
-        $("[data-test-id=phone] input").setValue(faker.phoneNumber().phoneNumber());
+        $("[data-test-id='date'] input").val(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(DataGenerator.generateName2());
+        $("[data-test-id=phone] input").setValue(DataGenerator.generatePhone());
         $("[data-test-id=agreement]").click();
 
         $x("//*[text()=\"Запланировать\"]").click();
-        $("[data-test-id=success-notification]").should(Condition.visible);
-        Duration.ofSeconds(15);                      //Загрузка не более 15 секунд
+        $("[data-test-id=success-notification]").should(visible);
 
-    }
-    @Test
-    public void shouldOrderCardIfNameWidthInvalidCharacter () {
-
-        $("[data-test-id=city] input").setValue(faker.address().city());
+        LocalDate localDate2 = LocalDate.now().plusDays(5);
+        DateTimeFormatter data2 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String strData2 = localDate2.format(data2);
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").val(strData);
-        $("[data-test-id=name] input").setValue("Фёдор");
-        $("[data-test-id=phone] input").setValue(faker.phoneNumber().phoneNumber());
-        $("[data-test-id=agreement]").click();
+        $("[data-test-id='date'] input").val(strData2);
+        $("div .button").click();
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldBe(visible).shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
 
-        $x("//*[text()=\"Запланировать\"]").click();
-        $("[data-test-id=success-notification]").should(Condition.visible);
-        Duration.ofSeconds(15);                      //Загрузка не более 15 секунд
+        $(".notification_status_error .button").click();
 
+        $(withText("Успешно!")).shouldBe(visible);
+        $("[data-test-id='success-notification'] .notification__content").shouldBe(visible).shouldHave
+                (exactText("Встреча успешно запланирована на " + strData2));
     }
 }
--- src/test/java/utils/DataGenerator.java
+++ src/test/java/utils/DataGenerator.java
@@ -2,27 +2,83 @@ package utils;
 
 import com.github.javafaker.Faker;
 import entitles.RegistrationInfo;
-import lombok.experimental.UtilityClass;
+import lombok.Value;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Random;
 
-
-@UtilityClass
 
 public class DataGenerator {
-
-    @UtilityClass
-
-
-    public static class Registration {
-        public static RegistrationInfo generateInfo(String locale) {
-            Faker faker = new Faker(new Locale(locale));
-            return new RegistrationInfo(faker.name().fullName(),
-                    faker.phoneNumber().phoneNumber(),
-                    faker.address().city());
-
-        }
-
+    private DataGenerator() {
     }
 
+    static Faker faker = new Faker(new Locale("ru"));
+
+    public static String generatePhone() {
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
+    }
+
+    public static String[] validNames = new String[]{
+            "Иванов Василий", "Петров Антон", "Смирнова Ольга", "Иванова Ирина"
+    };
+
+    public static String generateValidName() {
+        Random generator = new Random();
+        int randomIndex = generator.nextInt(3);
+        return validNames[randomIndex];
+    }
+
+    public static String[] names2 = new String[]{
+            "Иванов Артём", "Рина Зелёная", "Фёдор Достоевский"};
+
+
+    public static String generateName2() {
+        Random generator = new Random();
+        int randomIndex = generator.nextInt(2);
+        return names2[randomIndex];
+    }
+
+
+    public static String[] cities = new String[]{
+            "Казань", "Ставрополь", "Вологда", "Москва", "Санкт-Петербург", "Краснодар", "Ростов-на-Дону"
+    };
+
+    public static String generateCity() {
+        Random generator = new Random();
+        int randomIndex = generator.nextInt(6);
+        return cities[randomIndex];
+    }
+
+    public static String[] cities2 = new String[]{
+            "Сочи", "Новокузнецк", "Магнитогорск", "Нижневартовск"
+    };
+
+    public static String generateCity2() {
+        Random generator = new Random();
+        int randomIndex = generator.nextInt(3);
+        return cities2[randomIndex];
+    }
+
+
+    public static String generateDate(int days) {
+        String date = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+
+    public static RegistrationInfo getUser() {
+        RegistrationInfo user = new RegistrationInfo(generateValidName(), generatePhone(), generateCity());
+        return user;
+    }
+
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
 }
